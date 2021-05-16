73dcfac80f3ec69f0f78656a0114cc434e92a207
manoa <manoa@videorunrun.com>
2021-05-16 19:25:48 +0200

    wip

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,7 +3,7 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
     access_token: ENV["MESSENGER_ACCESS_TOKEN"],
-    subscribed_fields: %w[feed mention name]
+    subscribed_fields: %w[leadgen]
   )
 
 Faceboook::Messenger::Bot.on :message do |message|
