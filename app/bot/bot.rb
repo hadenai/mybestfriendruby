4adaa750a254d6653543aa81077148371ba29005
manoa <manoa@videorunrun.com>
2021-05-16 19:40:09 +0200

    wip

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,9 +1,11 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
+SUBSCRIBED_PARAMS = [ 'messages', 'message_echoes', 'message_deliveries', 'messaging_optins', 'leadgen' ]
+
 Facebook::Messenger::Subscriptions.subscribe(
     access_token: ENV["MESSENGER_ACCESS_TOKEN"],
-    subscribed_fields: []
+    subscribed_fields: SUBSCRIBED_PARAMS
   )
 
 Faceboook::Messenger::Bot.on :message do |message|
