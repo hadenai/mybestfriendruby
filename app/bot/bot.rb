5aa37f425623e345c8d4507b53865dfa6aca80b0
manoa <manoa@videorunrun.com>
2021-05-16 18:27:58 +0200

    WIP

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,10 +1,7 @@
 require 'facebook/messenger'
-require_relative './verification_provider'
 include Facebook::Messenger
 
-Facebook::Messenger.configure do |config|
-  config.provider = FacebookVerificationProvider.new
-end
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["MESSENGER_ACCESS_TOKEN"])
 
 Faceboook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
