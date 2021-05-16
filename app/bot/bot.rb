ef5ded373d54d985bc50696ba717ca6f0d012b0b
manoa <manoa@videorunrun.com>
2021-05-16 18:01:31 +0200

    WIP

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,9 +1,9 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
+
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: ENV['ACCESS_TOKEN'],
-  subscribed_fields: %w[feed mention name]
+  access_token: ENV['ACCESS_TOKEN'] = ENV['MESSENGER_ACCESS_TOKEN']
 )
 
 Faceboook::Messenger::Bot.on :message do |message|
